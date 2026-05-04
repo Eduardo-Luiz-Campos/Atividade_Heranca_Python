be43e2da3f0e9b79f195f3a26b266917621183a8
Eduardo-Luiz-Campos <campos.santos.eduardo@escola.pr.gov.br>
2026-05-04 11:41:59 +0000

    Trabalho Feito

---
+++ minha_garagem.py
@@ -0,0 +1,19 @@
+from veiculo import Carro, Moto
+
+v1 = Carro('Volkswagen', 'Gol', 2022, 4)
+v2 = Moto('Honda', 'CB', 2024, 471)
+v3 = Carro('Fiat', 'Uno Mini', 2013, 2)
+
+garagem = [v1, v2, v3]
+
+#Percorrer lista e imprimir tudo
+
+for v in garagem:
+    print(v)
+
+#Imprima quantos são carros e quantos são motos
+
+carros = [v for v in garagem if isinstance(v, Carro)]
+motos = [v for v in garagem if isinstance(v, Moto)]
+print(f'Carros: {len(carros)} | Motos: {len(motos)}')
+
---
+++ veiculo.py
@@ -0,0 +1,56 @@
+class Veiculo:
+    def __init__(self, marca, modelo, ano):
+        self.__marca = marca
+        self.__modelo = modelo
+        self.__ano = ano
+
+    @property
+    def marca(self): return self.__marca
+
+    @property
+    def modelo(self): return self.__modelo
+
+    @property
+    def ano(self): return self.__ano
+
+    def descrever(self):
+        return f'{self.__marca} {self.__modelo} ({self.__ano})'
+
+    def __str__ (self):
+        return self.descrever()
+
+class Carro(Veiculo):
+    def __init__ (self, marca, modelo, ano, num_portas):
+        super().__init__(marca, modelo, ano)
+        self.__num_portas = num_portas
+
+    @property
+    def num_portas(self): return self.__num_portas
+
+    def descrever(self):
+        base = super().descrever()
+        return f'{base} | {self.__num_portas} portas'
+
+
+class Moto(Veiculo):
+    def __init__ (self, marca, modelo, ano, cilindradas):
+        super().__init__(marca, modelo, ano)
+        self.__cilindradas = cilindradas
+
+    @property
+    def cilindradas(self): return self.__cilindradas
+
+    def descrever(self):
+        base = super().descrever()
+        return f'{base} | {self.__cilindradas} cc'
+
+#TESTES
+
+c = Carro('Toyota', 'Corolla', 2023, 4)
+m = Moto('Honda', 'CG 160', 2022, 160)
+
+print(c) #Toyota Corolla (2023) | 4 portas
+print(m) #Honda CG 160 (2022) | 160 cc
+print(c.marca)
+print(isinstance(c, Veiculo))
+print(isinstance(m, Carro))
